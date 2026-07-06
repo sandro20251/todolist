2178effedfc6a23837774d3e0719fd2c7528c2ea
Sandro <sandrocoelho2023a@gmail.com>
2026-07-06 17:30:02 -0300

    feat: conclusão da Sprint 3 (CRUD completo de tarefas)

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -4,5 +4,8 @@ const TaskController = require('../controllers/TaskController')
 // rota inicial
 router.get('/tasks', TaskController.getAll);
 router.post('/tasks', TaskController.create);
+router.get('/tasks/:id', TaskController.getOne);
+router.put('/tasks/:id', TaskController.update);
+router.delete('/tasks/:id', TaskController.delete2);
 
 module.exports = router;
