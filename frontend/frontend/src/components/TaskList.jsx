1543f67c1de5379353556a79d13164ec54fffde4
Sandro <sandrocoelho2023a@gmail.com>
2026-07-08 19:58:30 -0300

    feat: Sprint 5 concluída (filtros, pesquisa de tarefas)

--- frontend/frontend/src/components/TaskList.jsx
+++ frontend/frontend/src/components/TaskList.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import '../components/TaskList.css'
+
 
 const TaskList = ({ tasks, deleteExistingTask, updateExistingTask }) => {
 
     const [editingId, setEditingId] = useState(null);
     const [titulo, setTitulo] = useState("");
+    const [filtro, setFiltro] = useState("todas");
+    const [pesquisa, setPesquisa] = useState("")
+
 
     const iniciarEdicao = (id, titulo) => {
         setEditingId(id);
@@ -12,31 +17,79 @@ const TaskList = ({ tasks, deleteExistingTask, updateExistingTask }) => {
 
     const salvarEdicao = async (task) => {
 
-        const objeto = {
+        const updateTask = {
             title: titulo,
             completed: task.completed
         };
 
-        await updateExistingTask(task.id, objeto);
+        await updateExistingTask(task.id, updateTask);
 
         setTitulo("");
         setEditingId(null);
     };
 
+    const alterarStatus = async (task) => {
+        const objeto = {
+            title: task.title,
+            completed: !task.completed
+        }
+
+        await updateExistingTask(task.id, objeto);
+
+    }
+
+
+
+    const handleMudarFiltro = (tipo) => {
+        setFiltro(tipo)
+    }
+
+    let tarefasFiltradas = tasks;
+    if (filtro === "todas") {
+        tarefasFiltradas = tasks.filter((task) => task.title.includes(pesquisa))
+    }
+
+
+    if (filtro === "completas") {
+        tarefasFiltradas = tasks.filter((task) => task.completed && task.title.includes(pesquisa));
+    }
+
+    if (filtro === "incompletas") {
+        tarefasFiltradas = tasks.filter((task) => !task.completed && task.title.includes(pesquisa));
+    }
+
+
+
+
+
+
+
+
+
 
     return (
         <div>
             <h1>Minhas tarefas:</h1>
+            <h2>Faça sua pesquisa de tarefas aqui:</h2>
+            <input type="text" placeholder="O que você está procurando?" onChange={(e) => setPesquisa(e.target.value)} value={pesquisa} />
+            <br></br>
+            <button onClick={() => handleMudarFiltro("todas")}>Todas tarefas</button>
+            <button onClick={() => handleMudarFiltro("completas")}>Tarefas completas</button>
+            <button onClick={() => handleMudarFiltro("incompletas")}>Tarefas incompletas</button>
 
             {
-                tasks.map((task) => (
+                tarefasFiltradas.map((task) => (
 
-                    <div key={task.id}>
+                    <div key={task.id} className={task.completed ? 'tarefaCompleta' : 'tarefaIncompleta'}>
 
                         {
                             task.id === editingId ? (
 
                                 <div>
+                                    <input
+                                        type="checkbox"
+                                        checked={task.completed}
+                                        onChange={() => alterarStatus(task)} />
                                     <input
                                         type="text"
                                         value={titulo}
@@ -55,15 +108,19 @@ const TaskList = ({ tasks, deleteExistingTask, updateExistingTask }) => {
                             ) : (
 
                                 <div>
+                                    <input
+                                        type="checkbox"
+                                        checked={task.completed}
+                                        onChange={() => alterarStatus(task)} />
                                     <p>{task.title}</p>
 
-                                    <button 
+                                    <button
                                         onClick={() => deleteExistingTask(task.id)}
                                     >
                                         Excluir
                                     </button>
 
-                                    <button 
+                                    <button
                                         onClick={() => iniciarEdicao(task.id, task.title)}
                                     >
                                         Editar
