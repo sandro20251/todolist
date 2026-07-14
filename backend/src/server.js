5e9a5b334c10b551a9afea6f866950e32b7336e5
Sandro <sandrocoelho2023a@gmail.com>
2026-07-14 18:12:22 -0300

    backend todolist pronto para deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./database/index');
-const Task = require('./models/Task');
-// definições iniciais
-const PORTA = 5000;
+
 const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://todolist-two-gold-54.vercel.app'
+    ],
+    credentials: true
+}));
 
 // rotas
 app.use('/', require('./routes/taskRoutes'));
-// escuta de porta
-const iniciarServidor = async () => {
-    try {
-        await db.sync()
 
-        app.listen(PORTA, () => {
-            console.log(`O express está escutando a porta ${PORTA}`)
-        })
 
-    } catch (err) {
-        console.log(err.message)
-    }
-}
+// sincronizar banco
+db.sync()
+    .then(() => {
+        console.log("Banco conectado");
+    })
+    .catch((err) => {
+        console.log("Erro no banco", err.message);
+    });
 
-iniciarServidor();
+
+// exporta para o Vercel
+module.exports = app;
