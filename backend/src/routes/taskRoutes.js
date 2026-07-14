7654a2961392474394c8e77ad357709e64da12a1
Sandro <sandrocoelho2023a@gmail.com>
2026-07-14 19:14:50 -0300

    teste rota sem controller

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,11 +1,22 @@
+// const express = require('express');
+// const router = express.Router();
+// const TaskController = require('../controllers/TaskController')
+// // rota inicial
+// router.get('/tasks', TaskController.getAll);
+// router.post('/tasks', TaskController.create);
+// router.get('/tasks/:id', TaskController.getOne);
+// router.put('/tasks/:id', TaskController.update);
+// router.delete('/tasks/:id', TaskController.delete2);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
-const TaskController = require('../controllers/TaskController')
-// rota inicial
-router.get('/tasks', TaskController.getAll);
-router.post('/tasks', TaskController.create);
-router.get('/tasks/:id', TaskController.getOne);
-router.put('/tasks/:id', TaskController.update);
-router.delete('/tasks/:id', TaskController.delete2);
+
+router.get('/tasks', (req, res) => {
+    res.json({
+        teste: "rota funcionando"
+    });
+});
 
 module.exports = router;
