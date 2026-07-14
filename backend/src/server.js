9b8e3b5179c684a24f051703db9b82403b2aa951
Sandro <sandrocoelho2023a@gmail.com>
2026-07-14 19:08:02 -0300

    teste express puro no vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,40 +1,57 @@
+// const express = require('express');
+// const cors = require('cors');
+// const db = require('./database/index');
+
+// const app = express();
+
+// // middlewares
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         status: "API funcionando",
+//         ambiente: "Vercel"
+//     });
+// });
+
+// app.use(cors({
+//     origin: [
+//         'http://localhost:3000',
+//         'https://todolist-two-gold-54.vercel.app'
+//     ],
+//     credentials: true
+// }));
+
+// // rotas
+// app.use('/', require('./routes/taskRoutes'));
+
+
+// // sincronizar banco
+// db.authenticate()
+//     .then(() => {
+//         console.log("Banco conectado");
+//     })
+//     .catch((err) => {
+//         console.log("Erro banco:", err.message);
+//     });
+
+
+// // exporta para o Vercel
+// module.exports = app;
+
 const express = require('express');
 const cors = require('cors');
-const db = require('./database/index');
 
 const app = express();
 
-// middlewares
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
-        status: "API funcionando",
-        ambiente: "Vercel"
+        status: "API funcionando"
     });
 });
 
-app.use(cors({
-    origin: [
-        'http://localhost:3000',
-        'https://todolist-two-gold-54.vercel.app'
-    ],
-    credentials: true
-}));
-
-// rotas
-app.use('/', require('./routes/taskRoutes'));
-
-
-// sincronizar banco
-db.authenticate()
-    .then(() => {
-        console.log("Banco conectado");
-    })
-    .catch((err) => {
-        console.log("Erro banco:", err.message);
-    });
-
-
-// exporta para o Vercel
 module.exports = app;
