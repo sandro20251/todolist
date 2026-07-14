c2cc2efbd1b8388eacf77c152aafefeee3d51f5d
Sandro <sandrocoelho2023a@gmail.com>
2026-07-14 19:34:20 -0300

    teste model falso

--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -1,9 +1,15 @@
 const { DataTypes } = require('sequelize');
 const db = require('../database/index');
 
-const Task = db.define('Task', {
-    title: { type: DataTypes.STRING, allowNull: false },
-    completed: { type: DataTypes.BOOLEAN, defaultValue: false }
-})
+// const Task = db.define('Task', {
+//     title: { type: DataTypes.STRING, allowNull: false },
+//     completed: { type: DataTypes.BOOLEAN, defaultValue: false }
+// })
+
+class Task {
+
+}
+
+
 
 module.exports = Task;
