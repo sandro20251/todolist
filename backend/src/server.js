83eac5dc50d4de3d4515c17c8631ed81b52e9029
Sandro <sandrocoelho2023a@gmail.com>
2026-07-15 18:49:54 -0300

    commit railway 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,84 +1,6 @@
-// const express = require('express');
-// const cors = require('cors');
-// const db = require('./database/index');
-// process.on('uncaughtException', (err) => {
-//     console.error('UNCAUGHT EXCEPTION');
-//     console.error(err);
-// });
-
-// process.on('unhandledRejection', (err) => {
-//     console.error('UNHANDLED REJECTION');
-//     console.error(err);
-// });
-
-// console.log("1 - iniciou");
-
-// const express = require('express');
-// console.log("2 - express");
-
-// const cors = require('cors');
-// console.log("3 - cors");
-
-// const db = require('./database/index');
-// console.log("4 - database");
-
-// const taskRoutes = require('./routes/taskRoutes');
-// console.log("5 - routes");
-
-
-// const app = express();
-
-// // middlewares
-// app.use(express.json());
-
-// app.use(cors({
-//     origin: [
-//         'http://localhost:3000',
-//         'https://todolist-two-gold-54.vercel.app'
-//     ],
-//     credentials: true
-// }));
-
-// // rotas
-// app.use('/', require('./routes/taskRoutes'));
-
-
-// // sincronizar banco
-// db.authenticate()
-//     .then(() => {
-//         console.log("Banco conectado");
-//     })
-//     .catch((err) => {
-//         console.log("Erro banco:", err.message);
-//     });
-
-
-// // exporta para o Vercel
-// module.exports = app;
-
-process.on('uncaughtException', (err) => {
-    console.error('UNCAUGHT EXCEPTION');
-    console.error(err);
-});
-
-process.on('unhandledRejection', (err) => {
-    console.error('UNHANDLED REJECTION');
-    console.error(err);
-});
-
-console.log("1 - iniciou");
-
 const express = require('express');
-console.log("2 - express");
-
 const cors = require('cors');
-console.log("3 - cors");
-
 const db = require('./database/index');
-console.log("4 - database");
-
-const taskRoutes = require('./routes/taskRoutes');
-console.log("5 - routes");
 
 const app = express();
 
@@ -94,9 +16,10 @@ app.use(cors({
 }));
 
 // rotas
-app.use('/', taskRoutes);
+app.use('/', require('./routes/taskRoutes'));
 
-// banco
+
+// sincronizar banco
 db.authenticate()
     .then(() => {
         console.log("Banco conectado");
@@ -104,6 +27,14 @@ db.authenticate()
     .catch((err) => {
         console.log("Erro banco:", err.message);
     });
+const PORT = process.env.PORT || 3000;
 
+if (process.env.PORT || require.main === module) {
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Servidor rodando com sucesso na porta ${PORT}`);
+    });
+}
 
+// exporta para o Vercel
+module.exports = app;
 
