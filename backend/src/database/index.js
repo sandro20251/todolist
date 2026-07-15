3dd749e345c1de5bd00d1d29aa4fe552d0fcac07
Sandro <sandrocoelho2023a@gmail.com>
2026-07-15 18:58:59 -0300

    railway 4

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: false
+  process.env.DB_NAME,      // 'railway'
+  process.env.DB_USER,      // 'root'
+  process.env.DB_PASSWORD,  // 'FnxLwgILrRbMGEZjlSAGsMenTirlwjXi'
+  {
+    host: process.env.DB_HOST, // 'kodama.proxy.rlwy.net'
+    port: process.env.DB_PORT, // 43129
+    dialect: 'mysql',
+    logging: false, // para não poluir o terminal, se preferir
+    dialectOptions: {
+      connectTimeout: 60000 // Aumenta o tempo limite de conexão (muito útil em deploys)
     }
+  }
 );
 
 module.exports = sequelize;
